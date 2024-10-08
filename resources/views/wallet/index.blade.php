e3979d2bfba08078ab22ecdce7a81a76d495a9da
anucha1993 <108377127+anucha1993@users.noreply.github.com>
2024-10-08 13:13:33 +0700

    sss

--- resources/views/wallet/index.blade.php
+++ resources/views/wallet/index.blade.php
@@ -43,7 +43,7 @@
                                 @endforelse
                             </tbody>
                         </table>
-                        {!! $quotations->withQueryString()->links('pagination::bootstrap-5') !!}
+                       
                     </div>
                 </div>
             </div>
